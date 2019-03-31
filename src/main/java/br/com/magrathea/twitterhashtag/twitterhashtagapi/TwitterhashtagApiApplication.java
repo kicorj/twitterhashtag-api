bb5052ffab4457050a03306e758a5a47cb73400e
Francisco Cardoso <fcardosojr@gmail.com>
2019-03-31 20:10:01 -0300

    first commit

---
+++ src/main/java/br/com/magrathea/twitterhashtag/twitterhashtagapi/TwitterhashtagApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.magrathea.twitterhashtag.twitterhashtagapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterhashtagApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterhashtagApiApplication.class, args);
+	}
+
+}
